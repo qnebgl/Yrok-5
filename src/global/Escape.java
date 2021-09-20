c017b88575e68378231ea5a7f176c559bc8a044f
qnebgl <qnebgl@gmail.com>
2021-09-20 09:30:27 +0300

    kkkkkkk

---
+++ src/global/Escape.java
@@ -0,0 +1,14 @@
+package global;
+
+public class Escape {
+    public static void main(String[] args) {
+        String header = "\n\tНЬЮ-ЙОРК ПРОГНОЗ НА 3 ДНЯ:\n" ;
+        header += "\n\tДень\t\tМакс\tМин\tОсадки\n" ;
+        header += "\t----\t\t----\t---\t----------\n";
+        String forecast = "\tВоскресенье\t68F\t\t48F\tЯсно\n";
+        forecast += "\tПонедельник\t71F\t\t57F\tЯсно\n" ;
+        forecast +="\tВторник\t\t71F\t\t50F\tОблачность\n" ;
+        System.out.println(header + forecast);
+
+    }
+}
