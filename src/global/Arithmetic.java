6e4da8d161b06e622a59e57273574c5240d94fa0
qnebgl <qnebgl@gmail.com>
2021-09-20 10:16:32 +0300

    Arith

--- src/global/Arithmetic.java
+++ src/global/Arithmetic.java
@@ -7,7 +7,7 @@ class Arithmetic
        {
          int num = 100  ;
          int factor = 20    ;
-         int sum = 0 ;
+         int sum  ;
 sum = num + factor ; //100 + 20 
 System.out.println( "Результат сложения: " + sum ) ;
 sum = num - factor ;  //100 - 20 
