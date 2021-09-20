12b5739d75fe8d6cfa89475c7d29505217969d03
qnebgl <qnebgl@gmail.com>
2021-09-20 10:11:37 +0300

    Arith

---
+++ src/global/Arithmetic.java
@@ -0,0 +1,21 @@
+
+package global;
+class Arithmetic
+{
+
+       public static void main ( String[] args )
+       {
+         int num = 100  ;
+         int factor = 20    ;
+         int sum = 0 ;
+sum = num + factor ; //100 + 20 
+System.out.println( "Результат сложения: " + sum ) ;
+sum = num - factor ;  //100 - 20 
+System.out.println( "��������� ���������: " + sum ) ;
+sum = num * factor ; //100 * 20 
+System.out.println( "��������� ���������: " + sum ) ;
+sum = num / factor ; //100 / 20 
+System.out.println( "��������� �������: " + sum ) ;
+       }
+
+}
